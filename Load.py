38e5ae9043d0b025f3b824bb80e134771934d731
Oerlemans,Timo <t.oerlemans@student.fontys.nl>
2023-11-27 13:08:52 +0100

    Initial commit

---
+++ Load.py
@@ -0,0 +1,43 @@
+# first of all we need to import all the libraries that we need
+import streamlit as st
+from langchain import OpenAI
+from langchain.docstore.document import Document
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chains.summarize import load_summarize_chain
+
+# Function to generate response from the LLM model. This function will be called when the user
+# submits the form.
+def generate_response(txt):
+    # Instantiate the LLM model
+    llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
+    # Split text
+    text_splitter = CharacterTextSplitter()
+    texts = text_splitter.split_text(txt)
+    # Create multiple documents
+    docs = [Document(page_content=t) for t in texts]
+    # Text summarization
+    chain = load_summarize_chain(llm, chain_type='map_reduce')
+    return chain.run(docs)
+
+
+# Page title
+st.set_page_config(page_title='Text Summarization App')
+st.title('Text Summarization App')
+
+
+# Text input field
+txt_input = st.text_area('Your text:', '', height=200)
+
+
+# Form to accept user's text input for summarization
+result = []
+with st.form('summarize_form', clear_on_submit=True):
+    openai_api_key = st.text_input('OpenAI API Key', type = 'password', disabled=not txt_input)
+    submitted = st.form_submit_button('Generate Summary')
+    if submitted and openai_api_key.startswith('sk-'):
+        with st.spinner('Generating...'):
+            response = generate_response(txt_input)
+            result.append(response)
+            del openai_api_key
+if len(result):
+    st.info(response)
